4711ba55e698bb0f2f5e4ece5afc648d47af3fd0
Thanushh <thanush2112@gmail.com>
2022-11-21 20:44:04 +0530

    adding image and config files

---
+++ sample_config.js
@@ -0,0 +1,4 @@
+var config = {
+    WEATHER_API_KEY : "**ENTER WEATHER API KEY HERE", // from openweatherapi.org
+    CITY_BACKGROUND_KEY : "ENTER YOUR IMAGE SEARCH API KEY HERE", // JoJ Image Search from rapidapi.com
+}
